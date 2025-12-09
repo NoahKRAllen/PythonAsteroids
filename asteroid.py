90a82443c75e483190232ce4ca412ed1af767234
NoahKRAllen <NoahKRAllen@gmail.com>
2025-12-08 21:01:47 -0800

    Splitting asteroids, finished with basics

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 from constants import LINE_WIDTH
 import pygame
+from logger import log_event
+import random
 from circleshape import CircleShape
-
+from constants import ASTEROID_MIN_RADIUS
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -12,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        
+        print(self.velocity)
+        velocity_one = self.velocity.rotate(random_angle)
+        velocity_two = self.velocity.rotate(-random_angle)
+        print(f"Velocity one = {velocity_one}")
+        print(f"Velocity two = {velocity_two}")
+        smaller_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_one = Asteroid(self.position.x, self.position.y, smaller_radius)
+        asteroid_one.velocity = velocity_one * 1.2
+
+        asteroid_two = Asteroid(self.position.x, self.position.y, smaller_radius)
+        asteroid_two.velocity = velocity_two * 1.2
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
+from constants import *
 from shot import Shot
 import pygame
 class Player(CircleShape):
