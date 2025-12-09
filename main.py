5b2e849625046d39ea4b8e25d1da9d6bb8888440
NoahKRAllen <NoahKRAllen@gmail.com>
2025-12-08 19:55:57 -0800

    Collision is done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -30,6 +31,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+                
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
